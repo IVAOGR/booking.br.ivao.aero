3932d1f5dd49b6a0ef57db27f65cf065086eb4bc
Marios Aristodemou <56293658+GrMrWb@users.noreply.github.com>
2024-03-22 14:32:58 +0000

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -11,6 +11,6 @@ export const Logo: FunctionComponent<LogoProps> = ({ sidebar = false }) => {
     const imgUrl = Env[`LOGO${sidebar ? "_SIDEBAR_" : "_"}${themeVariant === ThemeVariants.DARK ? "DARK" : "LIGHT"}`];
 
     return (
-        <img src={imgUrl} width="214px" height="56px" className={`w-54 h-14 ${sidebar ? "" : "-ml-5"}`} alt="Logo IVAO Brasil" />
+        <img src="https://gr.ivao.aero/wp-content/uploads/elementor/thumbs/LogoWhite-pwr4kpg14wf50h46q7r6g9z5vno2z83s43rrdhalfk.png" width="214px" height="56px" className={`w-54 h-14 ${sidebar ? "" : "-ml-5"}`} alt="Logo IVAO Brasil" />
     );
 }
