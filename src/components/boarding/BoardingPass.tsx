8f11d66ee667b749bc6120ff33c15d10a17a159d
Marios Aristodemou <56293658+GrMrWb@users.noreply.github.com>
2024-09-06 18:19:00 +0100

    Update BoardingPass.tsx

--- src/components/boarding/BoardingPass.tsx
+++ src/components/boarding/BoardingPass.tsx
@@ -152,7 +152,9 @@ const BoardingPassLeftSide: FunctionComponent<BoardingPassProps> = ({
           <div className={bodyStyle.qrCodeInfo}>
             <div className={bodyStyle.infoText}>
               To participate in the event you must be aware of and willing to
-              comply with all guidelines available in the pilot briefing
+              comply with all guidelines available in the pilot briefing.
+
+              BE CONNECTED 10 minutes on the network before departure time, and ready for push back at least 5 minutes before
             </div>
 
             <QrCode value="https://gr.ivao.aero" size={76} renderAs="svg" />
